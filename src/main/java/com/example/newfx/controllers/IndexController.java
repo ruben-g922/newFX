5989f594d8b5b2a99dfad5f811877a0c2ea759aa
rgarcia-101 <ruben@garcia100.com>
2024-03-08 21:46:02 -0600

    Changed method to use the changeScene from Application class instead

--- src/main/java/com/example/newfx/controllers/IndexController.java
+++ src/main/java/com/example/newfx/controllers/IndexController.java
@@ -13,14 +13,11 @@ public class IndexController {
     private VBox window;
 
     /**
-     * Changes the scene to the main one
-     * Showcases one way of changing the scene
+     * Changes the scene to the content one
      */
     @FXML
-    protected void startButton() throws IOException {
-        Parent loader = FXMLLoader.load(Application.class.getResource("MenuPage.fxml"));
-        Application.getStage().setTitle("JavaFX");
-        Application.getStage().setScene(new Scene(loader, window.getWidth(), window.getHeight()));
+    protected void startButton(){
+        Application.changeScene("MenuPage.fxml", "JavaFX");
     }
 
     @FXML
