8e8cb4feb6f1cc48d72d90199fedf6559003bb4a
rgarcia-101 <ruben@garcia100.com>
2024-03-08 21:44:43 -0600

    Change scene method, javadoc

--- src/main/java/com/example/newfx/Application.java
+++ src/main/java/com/example/newfx/Application.java
@@ -1,6 +1,7 @@
 package com.example.newfx;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -8,7 +9,15 @@ import java.io.IOException;
 
 public class Application extends javafx.application.Application {
 
+    public static void main(String[] args) {
+        launch();
+    }
 
+
+    /** This static variable holds the main stage
+    The stage is the window, so it makes sense to only have a single main one
+    Having it be a static variable makes it easy to retrieve in other classes
+    Useful for when needing to change the stage's scene */
     private static Stage window;
 
     /**
@@ -27,13 +36,26 @@ public class Application extends javafx.application.Application {
     }
 
     /**
-     * Reduces hassle of finding where the current stage is
+     * Gets the main stage
      * @return Stage created at app startup
      */
     public static Stage getStage() {
         return window;
     }
-    public static void main(String[] args) {
-        launch();
+
+
+    /**
+     * Handles changing the scenes
+     * @param resource name of view to show
+     * @param title new title for the stage
+     */
+    public static void changeScene(String resource, String title) {
+        try {
+            Parent loader = FXMLLoader.load(Application.class.getResource(resource));
+            Application.getStage().setTitle(title);
+            Application.getStage().setScene(new Scene(loader, window.getWidth(), window.getHeight()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
